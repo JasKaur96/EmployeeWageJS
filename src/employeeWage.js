98639a8ebc6393d17eb0c4a1f94923efe5f1728f
JasKaur96 <jaspreetflora7@gmail.com>
2021-04-09 20:27:37 +0530

    REFACTORED: Used switch case to calculate daily wages.

--- src/employeeWage.js
+++ src/employeeWage.js
@@ -9,41 +9,27 @@ class EmployeeWage {
     print() {
         return "****** Welcome to EmployeeWage! ******";
      }
-    /*empcheck will store random value from 1 or 2.
-    On getting 1 it will display employee is present if the value is 2 then the employee is absent. */
-    attendanceCheck(){
-        this.empCheck = Math.floor(Math.random() * 10 ) % 3;
-        if(this.empCheck == IS_PRESENT){
-            console.log("\nEmployee is Present!")
-            this.attendance = "Present";
-            return this.attendance;
-        } 
-        else if(this.empCheck == IS_PART_TIME){
-                console.log("\nEmployee is Part-time Present!")
-                this.attendance = "Part-Time";
-                return this.attendance;
-        }
-        else{
-            console.log("\nEmployee is Absent!")
-            this.attendance = "Absent";
-            return this.attendance;
-        }
-    }   
-    /*attendanceCheck() is called here and on the basis of the return value dailywage of employee will be calculated.*/
+    /*empcheck will store random value from 1, 2 or 3.
+    It will display employee is present, parttime or absent and calculate its wages accordingly.*/
     dailyWage(){
-        this.attendanceCheck();
-        if(this.attendance == "Present"){
-            this.empHrs = 8;
+        this.empCheck = Math.floor(Math.random() * 10 ) % 3 + 1;
+        switch(this.empCheck){
+            case 1:
+                console.log("\nEmployee is Present!")
+                this.empHrs = 8;
+                break;
+            case 2:
+                console.log("\nEmployee is Part-time Present!")
+                this.empHrs = 4;
+                break;
+            case 3:
+                console.log("\nEmployee is Absent!")
+                this.empHrs = 0;
+                break;
         }
-        else if(this.attendance == "Part-Time"){
-            this.empHrs = 4;
-        }
-        else{
-            this.empHrs = 0;
-        }
-         this.salary = this.empHrs * EMP_RATE_PER_HR;
-         console.log("Employee Daily Wage :"+this.salary);
-    }
+        this.salary = this.empHrs * EMP_RATE_PER_HR;
+        console.log("Employee Daily Wage :"+this.salary);
+    }    
   }
   
 let display = function(){
--- test/testEmp.js
+++
@@ -1,7 +0,0 @@
-const assert = require ('chai').assert;
-const utils = require('../src/employeeWage')
-describe('Testing Employee Wage.', function(){
-    it('first name should return true', function(){
-        assert.equal(utils.firstName("Jaspreet"),true)  
-    })
-})
