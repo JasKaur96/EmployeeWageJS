f040c330721916a62fbc75ea0dead9d2104270f7
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-04-11 15:58:53 +0530

    REFACTORED: Added parttime emp and calculated daily wage.

--- test/testEmp.js
+++ test/testEmp.js
@@ -1,7 +1,23 @@
 const assert = require ('chai').assert;
-const utils = require('../src/employeeWage')
-describe('Testing Employee Wage.', function(){
-    it('first name should return true', function(){
-        assert.equal(utils.firstName("Jaspreet"),true)  
+const expect = require('chai').expect //getting from asserting lib
+const employeeWage = require('../src/employeeWage')
+const constants = require('../src/constants');
+const wage = require('../src/wageCalculation')
+const employee = new employeeWage.EmployeeWage();
+const wageCalculation = new wage.WageCalculation();
+
+describe('******Testing Employee Wage.******', function(){
+   
+    it('Should return true on attendance check', function(){
+        const result = employee.attendanceCheck();
+        assert.equal(result,true)  ;
+    })
+
+    it('Should return true on dailywage', function(){
+        assert.equal(wageCalculation.dailyWage("Present"),160);
+    })
+
+    it('Should return true on dailywage', function(){
+        assert.true(wageCalculation.dailyWage("Part-Time"),80);
     })
 })
