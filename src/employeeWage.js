b9b750a9a86ff04585d8138a2a2313844b1dfe4d
JasKaur96 <jaspreetflora7@gmail.com>
2021-04-12 11:12:49 +0530

    REFACTORED: reaching monthly working hrs or working days.

--- src/employeeWage.js
+++ src/employeeWage.js
@@ -1,32 +1,48 @@
 const constants = require('./constants')
-let empCheck = 0;
-let attendance;
+const wageCalculation = require('../src/wageCalculation')
+const wage = new wageCalculation.WageCalculation();
+let totalEmpHrs = 0, empDailyWageArr= [];
+let totalEmpWage = 0;
+let empHrs = 0,totalWorkingDays = 0;
+let empDailyWAgeMap = new Map();
 class EmployeeWage {
     constructor() {}
 
     print() {return "****** Welcome to EmployeeWage! ******";}
-    /*empcheck will store random value from 1 or 2.
-    On getting 1 it will display employee is present if the value is 2 then the employee is absent. */
-    attendanceCheck(){
-        empCheck = Math.floor(Math.random() * 10 ) % 2;
-        if(empCheck == constants.IS_PRESENT){
-            console.log("\nEmployee is Present!")
-            attendance = "Present";
-            return true;
-        } 
-        else{
-            console.log("\nEmployee is Absent!")
-            attendance = "Absent";
-            return false;
-        }
-    }   
+
+    attendanceCheck(empCheck){
+           switch(empCheck){
+                case 1:
+                    empHrs = 8;
+                    break;
+                case 2:
+                    empHrs = 4;
+                    break;
+                case 3:
+                    empHrs = 0;
+                    break;
+            } 
+            return empHrs;
+    }
+
+    wageCalculation(){
+        while (totalEmpHrs < constants.MAX_HRS_IN_MONTH && totalWorkingDays < constants.NUM_OF_WORKING_DAYS){
+            totalWorkingDays++;
+            let empCheck = Math.floor(Math.random() * 10 ) % 3;
+            totalEmpHrs = totalEmpHrs + this.attendanceCheck(empCheck);
+            empDailyWageArr.push(wage.calDailyWage(totalEmpHrs));
+        }      
+        let empWage = wage.calDailyWage(totalEmpHrs);
+        return "\nDAY:"+  totalWorkingDays + " EmpHr:" + totalEmpHrs + " EmpWage:" + empWage;  
+    }    
 }
 
 let display = function(){
-    console.log( "****** Welcome to EmployeeWage! ******");
-    let employeeWage = new EmployeeWage().attendanceCheck();
+    console.log("****** Welcome to EmployeeWage! ******");
+    let employeeWage = new EmployeeWage().wageCalculation();
     console.log(employeeWage);
 }
 display();
+
 
 module.exports = {display , EmployeeWage};
--- test/testEmp.js
+++ test/testEmp.js
@@ -1,12 +1,34 @@
 const assert = require ('chai').assert;
+const expect = require('chai').expect //getting from asserting lib
 const employeeWage = require('../src/employeeWage')
 const constants = require('../src/constants');
+const wageCalculation = require('../src/wageCalculation')
 const employee = new employeeWage.EmployeeWage();
+const wage = new wageCalculation.WageCalculation();
 
 describe('******Testing Employee Wage.******', function(){
    
     it('Should return true on attendance check', function(){
         const result = employee.attendanceCheck();
-        assert.equal(result,true)  ;
+        assert.equal(result,8)  ;
     })
+
+    it('Should return true on dailywage', function(){
+        const result = wage.dailyWageCalculation("Present");
+        assert.equal(result,true);
+    })
+
+    it('DailyWage for Part-time employee', function(){
+        assert.equal(wage.dailyWageCalculation("Part-Time"),true);
+    })
+
+    it('Should return true on returning numeric value.', function(){
+        assert.isBoolean(wage.dailyWageCalculation("Part-Time"),true);
+    })
+
+    it('Calculate Monthly Wage.', function(){
+        let sal = 160 * 20
+        assert.equal(wage.monthlyWageCalculation(sal),3200);
+    })
+    
 })
